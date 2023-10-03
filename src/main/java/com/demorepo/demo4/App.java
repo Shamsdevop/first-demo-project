57dedb09c26fa3dabbab3ec9a64c4f732dd83bfc
shams <shamsshah26@gmail.com>
2023-10-03 23:51:58 +0100

    project is updated

---
+++ src/main/java/com/demorepo/demo4/App.java
@@ -0,0 +1,13 @@
+package com.demorepo.demo4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
